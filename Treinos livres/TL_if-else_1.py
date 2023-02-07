b88ee9311b2c0e0732724e18fbd9538b81f8fc5c
hatesun <dktkd1@gmail.com>
2023-02-07 18:55:26 -0300

    melhoramento no código

--- Treinos livres/TL_if-else_1.py
+++ Treinos livres/TL_if-else_1.py
@@ -3,7 +3,8 @@
 # Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
 letra = str(input("Digite 1 letra: ")).strip().upper()
-if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+# if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+if letra in 'A E I O U':
     print(f"A letra ({letra}) digitada é uma vogal.")
 else:
     print(f"A letra ({letra}) digitada é uma consoante.")
