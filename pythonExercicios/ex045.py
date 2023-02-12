af1451014188bfd5e6073b52564f4dd66a9c7eaa
hatesun <dktkd1@gmail.com>
2023-02-11 21:07:08 -0300

    edit

--- pythonExercicios/ex045.py
+++ pythonExercicios/ex045.py
@@ -22,7 +22,7 @@ while jogador > 2:
         "[2]Tesoura\n"))
 
 # loading
-print("Processando...")
+print("JO...KEN...PÔ!")
 sleep(1.3)
 
 # escolhas do jogador
@@ -43,10 +43,13 @@ elif cpu == 2:
 # saída
 print(f"Você escolheu {jog} e eu escolhi {cp}")
 if jogador == cpu:
-    print("Empatou")
+    print("\033[33mEmpatou\033[m")
 elif (jogador == 0 and cpu == 2) or (jogador == 1 and cpu == 0) or (jogador == 2 and cpu == 1):
-    print("Ganhei. Mais sorte na próxima vez!")
+    print("\033[31mGanhei\033[m. Mais sorte na próxima vez!")
 elif (jogador == 0 and cpu == 1) or (jogador == 1 and cpu == 2) or (jogador == 2 and cpu == 0):
-    print("Parabéns, você venceu!")
+    print("\033[32mParabéns\033[m, você venceu!")
     
 print("-=-" * 20)
+
+# melhorar code
+# ultizar itens
