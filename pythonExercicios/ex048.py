0f5440cd54e135546a75d6696807d08ca0967562
hatesun <dktkd1@gmail.com>
2023-02-13 22:17:04 -0300

    commit

---
+++ pythonExercicios/ex048.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que calcule a SOMA entre todos os
+NÚMEROS ÍMPARES que são MULTIPLOS DE TRÊS e que
+se encontram no intervalo de 1 até 500.
+
+"""
+
+for n in range(0, 10, 3):
+    if n % 2 != 0:
+        s = 0 + n
+print(s)
