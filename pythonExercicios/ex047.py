5e82632ac319530b21b1a017434cb31e66d6916c
hatesun <dktkd1@gmail.com>
2023-02-13 21:52:44 -0300

    commit

---
+++ pythonExercicios/ex047.py
@@ -0,0 +1,6 @@
+"""Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50."""
+
+print("-="*10,"Lista de pares entre 1 e 50:","-="*10)
+for n in range(2,49, 2):
+    print(n)
+print("-="*13,"Fim da lista.","-="*13)
