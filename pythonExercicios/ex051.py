2368323240130318ab5b19552c4078254e55f908
hatesun <dktkd1@gmail.com>
2023-02-14 20:00:13 -0300

    commit

---
+++ pythonExercicios/ex051.py
@@ -0,0 +1,10 @@
+"""
+Desenvolva um programa que leia o PRIMEIRO TERMO e a RAZÃO de uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.
+"""
+
+print("-="*10,"Progressão Aritmética","-="*10)
+a1 = int(input("Digite o primeiro termo da PA: "))
+r = int(input("Digite a razão da PA: "))
+for c in range(a1, r*10+1 , r):
+    print(c)
