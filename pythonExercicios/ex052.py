adfd92f4a0000564c5f7e01bc1250f3dccd11269
hatesun <dktkd1@gmail.com>
2023-02-14 21:19:19 -0300

    commit

---
+++ pythonExercicios/ex052.py
@@ -0,0 +1,8 @@
+"""
+Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+"""
+
+n = int(input("Digite um número: "))
+for c in range(0,n+1):
+    if c >
+    print(c, n)
