51fb00bf3a3d1434cfffd3b98462e51eeef5f117
hatesun <dktkd1@gmail.com>
2023-03-07 22:01:53 -0300

    ex60

--- pythonExercicios/ex060.py
+++ pythonExercicios/ex060.py
@@ -3,3 +3,16 @@ Faça um programa que leia um número qualquer e mostre seu fatorial.
 ex:
 5! = 5x4x3x2x1=120
 """
+
+num = int(input("Digite um número para calcular seu numero fatorial: "))
+n1 = num
+fat = num -1
+n = 1
+if num <= 1:
+    print(f"O número fatorial de {num} é 1.")
+else:
+    while n < n1:
+        n += 1 
+        num = num * fat
+        fat = fat - 1 
+print(num)
