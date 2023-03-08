afabac191fb7b003749521f760406d2c55c4fadf
hatesun <dktkd1@gmail.com>
2023-03-07 21:11:36 -0300

    ex58

--- pythonExercicios/ex058.py
+++ pythonExercicios/ex058.py
@@ -12,24 +12,34 @@ tentativas = 1
 print("-=-" * 20)
 print("Vou pensar em um número entre 0 e 10. Tente adivinhar...")
 print("-=-" * 20)
+# print(cpu) # - CHEAT -
 jogador = int(input("Em que número pensei? "))
 while jogador > 10 or jogador < 0:
     jogador = int(input("Número digitado inválido. Por favor, digite um número entre 0 e 10: "))
 print("Processando...")
 sleep(1.3)
-while jogador != cpu:
-    print("\033[31mNúmero errado\033[m.")
-    # print(cpu) # cheater
-    novo = input("Tentar novamente (S/N)? ").upper()
-    while novo != "S":
-        novo = input("Opção inválida. Deseja tentar novamente (S/N)? ").upper()
-    if novo == "S":
-        tentativas += 1
-        jogador = int(input("Em que número pensei? "))
-        while 0 > jogador > 10 :
-            jogador = int(input("Número digitado inválido. Por favor, digite um número entre 0 e 10: "))
-    else:
-        print(f"Você saiu com {tentativas} tentativas.")
-        exit()
-print("\033[32mAcertou o número! Parabéns\033[m")
-print(f"Voce acertou com {tentativas} tentativas.")
+if jogador == cpu:
+    print("\033[32mIMPRESSIONANTE! Você acertou o número de primeira!! Muito bem! Parabéns!\033[m")
+    exit()
+else:
+    while jogador != cpu:
+        print("\033[31mNúmero errado\033[m.")
+        novo = input("Tentar novamente (S/N)? ").upper()
+        while novo != 'S' and novo != 'N':
+            novo = input("Opção inválida. Deseja tentar novamente (S/N)? ").upper()
+        if novo == "S":
+            tentativas += 1
+            jogador = int(input("Em que número pensei? "))
+            while jogador > 10 or jogador < 0:
+                jogador = int(input("Número digitado inválido. Por favor, digite um número entre 0 e 10: "))
+            print("Processando....")
+            sleep(1.3)
+        else:
+            if tentativas > 1:
+                print(f"Que pena, você não acertou. Você saiu com {tentativas} tentativas.")
+            else:
+                print(f"Que pena, você não acertou. Você saiu com {tentativas} tentativa.")
+            exit()
+    print("\033[32mAcertou o número! Parabéns!\033[m")
+    print(f"Você acertou com {tentativas} tentativas.")
+    exit()
