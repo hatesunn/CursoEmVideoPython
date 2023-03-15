e3bca2c1eeb119255d1d6bd6a58841333a308995
hatesun <dktkd1@gmail.com>
2023-03-14 21:50:00 -0300

    resolucao

--- pythonExercicios/ex061.py
+++ pythonExercicios/ex061.py
@@ -2,3 +2,13 @@
 Refaca o ex051, lendo o PRIMEIRO TERMO e a RAZÃO de uma PA,
 mostrando os 10 primeiros termos da progressão usando a estrutura While
 """
+
+print("-="*10,"Progressão Aritmética","-="*10)
+a1 = int(input("Digite o primeiro termo da PA: "))
+r = int(input("Digite a razão da PA: "))
+a10 = 0
+while a10 < 10:
+   print (a1, end="-> ")
+   a1 = a1 + r
+   a10 += 1
+print("Acabou")
--- pythonExercicios/ex062.py
+++ pythonExercicios/ex062.py
@@ -2,3 +2,27 @@
 Melhore o ex061, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerra quando ele disser que quer mostrar 0 termos
 """
+
+print("-="*10,"Progressão Aritmética","-="*10)
+
+a1 = int(input("Digite o primeiro termo da PA: "))
+r = int(input("Digite a razão da PA: "))
+a10 = 0
+while a10 < 10:
+   print (a1, end="-> ")
+   a1 = a1 + r
+   a10 += 1
+print("Acabou")
+cont = 1
+while cont != 0:
+    cont = int(input("Mais quantos termos deseja(0 para sair)? "))
+    if cont == 0:
+        print("Saindo...")
+        exit()
+    else:
+        a10 = 0
+        while a10 < cont:
+            print (a1, end="-> ")
+            a1 = a1 + r
+            a10 += 1
+        print("Acabou")
