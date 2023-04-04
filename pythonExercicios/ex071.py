8530fd85ac8e9a265d8ca05133ba57863d76fdcf
hatesun <dktkd1@gmail.com>
2023-04-04 08:11:54 -0300

    saque caixa eletronico

--- pythonExercicios/ex071.py
+++ pythonExercicios/ex071.py
@@ -4,3 +4,24 @@ a ser sacado(número inteiro) e o programa vai informar quantas cédulas de cada
 Obs: Considere que o caixa possui cédulas de 50, 20,10 e 1.
 """
 
+valor = int(input("Qual o valor do saque? R$ "))
+total = valor
+ced = 50
+total_ced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        total_ced += 1
+    else:
+        if total_ced > 0:
+            print(f"Total de {total_ced} cédulas de R$ {ced}")
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        total_ced = 0
+        if total == 0:
+            break
+print("Finalizando...")
