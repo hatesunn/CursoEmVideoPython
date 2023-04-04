c63a33442d725665f7f1b9eabb7639bdbf2b07b1
hatesun <dktkd1@gmail.com>
2023-04-04 07:04:37 -0300

    primeiro ex de break

--- pythonExercicios/ex066.py
+++ pythonExercicios/ex066.py
@@ -4,8 +4,8 @@ o usuário digitar o valor 999, que é a condição de parada.
 No final, mostre quantos números foram digitados e qual foi a soma entre eles(desconsiderando o flag).
 """
 
-n = s = c = 0
-while n != 999:
+s = c = 0
+while True:
     n = int(input("Digite um número: "))
     if n == 999:
         break
