bb8000001bf26e579aa59264b31b2ca94badf2e1
hatesun <dktkd1@gmail.com>
2023-04-04 13:43:40 -0300

    Times futebol

--- pythonExercicios/ex073.py
+++ pythonExercicios/ex073.py
@@ -28,5 +28,14 @@ times = (
 "Avai",
 "Juventude"
 )
-print("{:-^140}".format("Times do Brasileirão Série A"))
+print("\n{:=^140}".format(" \033[032mTimes do Brasileirão Série A\033[m "))
 print(times)
+print("{:=^140}".format(" \033[032mOs 5 primeiros times\033[m "))
+print(times[:5])
+print("{:=^140}".format(" \033[032mOs 4 na zona de rebaixamento\033[m "))
+print(times[-5:-1])
+print("{:=^140}".format(" \033[032mOrdem alfabética\033[m "))
+print(sorted(times))
+print("{:=^140}".format(" \033[032mO Sao Paulo está na posição\033[m "))
+print(times.index("Sao Paulo")+ 1,"ª posição")
+print("{:=^140}".format(" \033[032m Fim do programa \033[m "))
