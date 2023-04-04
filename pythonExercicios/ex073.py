b604c5a2ff944a64d6ee5db2b2d42f8e596a84a6
hatesun <dktkd1@gmail.com>
2023-04-04 13:19:41 -0300

    times

--- pythonExercicios/ex073.py
+++ pythonExercicios/ex073.py
@@ -1,8 +1,32 @@
 """
-Crie uma tupla preenchida com os 20 primeiros colocados da tablea do CB,
-na ordem de colocação. Depois mostre:
+Crie uma tupla preenchida com os 20 primeiros colocados da tablea do CB, na ordem de colocação. Depois mostre:
 A) Apenas o 5 primeiros colocados.
 B) Os últimos 4 colocados da tabela.
 C) Uma lista com os times eme ordem alfabética.
 D) Em que posicão na tabela está o time SP.
 """
+
+times = (
+"Palmeiras",
+"Internacional",
+"Fluminense",
+"Corinthians",
+"Flamengo",
+"Athletico-PR",
+"Atletico-MG",
+"Fortaleza",
+"Sao Paulo",
+"America-MG",
+"Botafogo",
+"Santos",
+"Goias",
+"Bragantino",
+"Coritiba",
+"Cuiaba",
+"Ceara-SC",
+"Atletico-GO",
+"Avai",
+"Juventude"
+)
+print("{:-^140}".format("Times do Brasileirão Série A"))
+print(times)
