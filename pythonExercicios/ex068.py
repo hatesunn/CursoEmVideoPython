5d2734adb59f3fe1e2a2a031bf1ded6110660686
hatesun <dktkd1@gmail.com>
2023-04-04 07:21:01 -0300

    Jogo par ou impar

--- pythonExercicios/ex068.py
+++ pythonExercicios/ex068.py
@@ -7,25 +7,23 @@ import random
 vitorias = 0
 while True:
     n = int(input("Digite um valor: "))
-    par_impar = str(input("Você quer par ou ímpar? ")).upper()
-    par_impar = par_impar[0]
+    par_impar = ""
+    while par_impar not in "PI":
+        par_impar = str(input("Você quer par ou ímpar? ")).strip.upper()[0]
     cpu = random.randint(1,2)
     soma = n + cpu
-    print(cpu)
-    if soma % 2 == 1:
-        resultado = 1 #impar
-    else:
-        resultado = 0 #par
-    if resultado == 1 and par_impar == "I":
-        print("\033[32mAcertou!\033[m")
-        vitorias += 1
-    elif resultado == 0 and par_impar == "P":
-        print("\033[32mAcertou!\033[m")
-        vitorias += 1
-    elif resultado == 1 and par_impar == "P":
-        print("\033[31mErrou!\033[m")
-        break
-    elif resultado == 0 and par_impar == "I":
-        print("\033[31mErrou!\033[m")
-        break
+    if par_impar == "P":
+        if soma % 2 == 0:
+            print("\033[32mAcertou!\033[m")
+            vitorias += 1
+        else:
+            print("\033[31mErrou!\033[m")
+            break
+    if par_impar == "I":
+        if soma % 2 == 1:
+            print("\033[32mAcertou!\033[m")
+            vitorias += 1
+        else:
+            print("\033[31mErrou!\033[m")
+            break
 print(f"Voce teve {vitorias} vitórias seguidas.")
