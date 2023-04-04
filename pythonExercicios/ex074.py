8f0308e64e1aac69bf28c84c4c1c18b6779670db
hatesun <dktkd1@gmail.com>
2023-04-04 14:46:02 -0300

    5 num random max e min

--- pythonExercicios/ex074.py
+++ pythonExercicios/ex074.py
@@ -3,3 +3,14 @@ Crie um programa que vai gerar cinco números aleatórios e colocar em uma Tupla
 Depois disso, mostre a listagem de números gerados e também indique o menor e o maior
 valor que estão na tupla.
 """
+
+from random import randint
+a = randint(0,10)
+b = randint(0,10)
+c = randint(0,10)
+d = randint(0,10)
+e = randint(0,10)
+f = (a , b, c, d, e)
+print(f"5 numeros aleatórios: {f}")
+print(f"O menor número é : {min(f)}")
+print(f"O maior número é {max(f)}")
