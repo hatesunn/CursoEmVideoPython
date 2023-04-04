5620b08a17ceb860331b0399ae1df6bcf86b6ea6
hatesun <dktkd1@gmail.com>
2023-04-04 15:32:17 -0300

    Lendo 4 valores

--- pythonExercicios/ex075.py
+++ pythonExercicios/ex075.py
@@ -5,3 +5,13 @@ A) Quantas vezes apareceu o valor 9;
 B) Em que posição foi digitado o primeiro valor 3;
 c) Quais foram os números pares.
 """
+
+n1 = 9
+n2 = 3
+n3 = 5
+n4 = 0
+num = (n1, n2, n3, n4)
+if num % 2 == 0:
+    print(num)
+
+print(num)
