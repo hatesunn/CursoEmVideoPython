552273498753b033f3f43c5dc9de6a2b1967f482
hatesun <dktkd1@gmail.com>
2023-04-04 07:36:37 -0300

    ex de idade e sexo

--- pythonExercicios/ex069.py
+++ pythonExercicios/ex069.py
@@ -9,24 +9,19 @@ C) Quantas mulheres tem menos de 20 anos.
 maioridade = h = m = 0
 cont = "S"
 while True:
-    idade = int(input("Digite sua idade: "))
-    sexo = str(input("Digite seu sexo(M/F): ")).upper()
-    sexo = sexo[0]
+    idade = int(input("Digite idade: "))
+    sexo = " "
     while sexo not in "MF":
-        sexo = str(input("Digite seu sexo(M/F): ")).upper()
-        sexo = [0]
+        sexo = str(input("Digite sexo(M/F): ")).strip().upper()[0]
     if idade > 18:
         maioridade += 1
     if sexo  == "M":
         h += 1
     if sexo == "F" and idade < 20:
         m += 1
-    cont = str(input("Deseja continuar(S/N)? ")).upper()
-    cont = cont[0]
+    cont = input("Deseja continuar(S/N)? ").strip().upper()[0]
     while cont not in "SN":
-        cont = str(input("Deseja continuar(S/N)? ")).upper()
-        cont = cont[0]
+        cont = input("Deseja continuar(S/N)? ").strip().upper()[0]
     if cont == "N":
         break
-    
 print(f"Teve {maioridade} maiores de idade, {h} homens e {m} mulheres com menos que 20 anos.")
