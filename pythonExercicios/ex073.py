8a3d1df94614dd7bef995023259b2bda4ee36c87
hatesun <dktkd1@gmail.com>
2023-04-04 11:14:19 -0300

    novos

---
+++ pythonExercicios/ex073.py
@@ -0,0 +1,8 @@
+"""
+Crie uma tupla preenchida com os 20 primeiros colocados da tablea do CB,
+na ordem de colocação. Depois mostre:
+A) Apenas o 5 primeiros colocados.
+B) Os últimos 4 colocados da tabela.
+C) Uma lista com os times eme ordem alfabética.
+D) Em que posicão na tabela está o time SP.
+"""
