7969f43ebd41cd19a12df9e323ec6c1262384f83
hatesun <dktkd1@gmail.com>
2023-04-04 09:16:33 -0300

    aula

---
+++ pythonTeste/aula016.py
@@ -0,0 +1,26 @@
+# lanche = ("Hambúrguer", "Suco", "Pizza", "Pudim", "Batata frita")
+
+# # for comida in lanche:
+# #     print(lanche)
+
+# # # for c in range(0, len(lanche)):
+# # #     print(f"Eu vou comer {lanche[c]} na posição {c}")
+
+# # for pos, comida in enumerate (lanche):
+# #     print(f"Eu vou comer {comida} na posicao {pos}")
+# # print("Comi muito!")
+
+# print(sorted(lanche))
+# print(lanche)
+
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = b + a
+# print(c)
+# print(c.index(5,1))
+
+
+pessoa = ("Anderson", 33, "M", 100)
+
+print(pessoa)
