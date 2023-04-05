0cf41948ab95610d1574fddd2513eba31df6a00d
hatesun <dktkd1@gmail.com>
2023-04-05 08:17:16 -0300

    vogais em palavras

--- pythonExercicios/ex077.py
+++ pythonExercicios/ex077.py
@@ -2,3 +2,13 @@
 Crie um programa que tenha uma tupla com várias palavras(não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são suas vogais.
 """
+
+palavras = ("aprender", "Programar", "Linguagem","Python")
+print(palavras)
+for palavras in palavras:
+    m = palavras.upper()
+    v = " "
+    for l in palavras:
+        if l in 'aeiou':
+            v += l
+    print(f"Vogais em {m}: {v}")
