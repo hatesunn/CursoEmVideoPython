309f244faaf38bdec61ee115d9e10c5bf05bd3c7
hatesun <dktkd1@gmail.com>
2023-04-11 07:34:58 -0300

    números digitados e ordem crescente

--- pythonExercicios/ex079.py
+++ pythonExercicios/ex079.py
@@ -3,3 +3,28 @@ Crie um programa onde um usário possa digitar vários valores numéricos e cada
 Caso o número já exista lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitado em ordem crescente.
 """
+
+lista = []
+cont = 0
+while cont != 2:
+    if cont == 2:
+        break
+    while True:
+        n = int(input("Digite um número: "))
+        if n in lista:
+            print("Número já foi digitado. Por favor insira outro número.")
+        else:
+            lista.append(n)
+            break
+    while True:
+        cont = int(input("""
+Deseja continuar?
+[1] Sim
+[2] Não
+"""))
+        if cont == 1 or cont == 2:
+            break
+        else:
+            print("Opção inválida. Escolha uma opção válida.")      
+print(f"Os números digitados foram: {lista}")
+print(f"Os valores em ordem crescente é: {sorted(lista)}")
