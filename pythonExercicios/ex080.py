e677a5be697cfaa7ce62dbafbcb08752b203269c
hatesun <dktkd1@gmail.com>
2023-04-11 09:06:04 -0300

    ex80

--- pythonExercicios/ex080.py
+++ pythonExercicios/ex080.py
@@ -3,3 +3,9 @@ Crie um programa onde o usuário possa digitar 5 valores numéricos e cadastre-o
 já na posição correta de inserção(sem usar o sort()).
 No final, mostre a lista ordenada na tela.
 """
+
+num = []
+for n in range(5):
+    i = int(input(f"Digite um número: "))
+    if i > 
+print(num)
