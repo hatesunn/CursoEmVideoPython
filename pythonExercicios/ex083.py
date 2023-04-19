86ab2350229c414a6ef17a14fa3a624d683d32e2
macos <dktkd1@gmail.com>
2023-04-19 06:42:26 -0300

    verificar expressao

--- pythonExercicios/ex083.py
+++ pythonExercicios/ex083.py
@@ -2,3 +2,19 @@
 Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada
 está com os parênteses abertos e fechados na ordem correta.
 """
+
+exp = input("Digite uma expressão: ")
+pilha = []
+for pare in exp:
+    if pare == "(":
+        pilha.append("(")
+    elif pare == ")":
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(")")
+            break
+if len(pilha) == 0:
+    print("Expressão correta!")
+else:
+    print("Expressão errada!")
