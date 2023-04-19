1b6dbdfcd4cdd1eb57d26f75a389833f9f4e745a
hatesun <dktkd1@gmail.com>
2023-04-19 07:25:57 -0300

    Listas de exercicios

---
+++ pythonExercicios/ex0086.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
+0[][][]
+1[][][]
+2[][][]
+ 0 1 2
+No final, mostre a matriz com a formatação correta
+"""
