f5b3e0e67e53d1f8a84a51cdf50f37ff70a583db
hatesun <dktkd1@gmail.com>
2023-04-20 07:02:42 -0300

    ler 7 numeros e dividir entre par e impar

--- pythonExercicios/ex085.py
+++ pythonExercicios/ex085.py
@@ -1,4 +1,18 @@
 """
-Criue um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados
+Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados
 os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 """
+par = []
+impar = []
+lista = [par, impar]
+for n in range(7):
+    num = int(input(f"Digite o {n+1} número: "))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+par.sort()
+impar.sort()
+print(lista)
+print(f"Os números pares digitados foram: {par}")
+print(f"Os números ímpares digitadso foram: {impar}")
